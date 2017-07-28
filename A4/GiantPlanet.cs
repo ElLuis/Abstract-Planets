99522b98e3146fd3989b4accfea24348fe8a67e9
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2017-07-28 15:25:04 -0400

    Interfaces and subclass of Planet

---
+++ A4/GiantPlanet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4
+{
+    public interface HasMoons
+    {
+        bool CountMoon();
+    }
+
+    public interface HasRings
+    {
+        bool CountRing();
+    }
+
+    class GiantPlanet : Planet
+    {
+        private string _type; //either Gas or Ice
+        public string Type { get { return _type; } set { _type = value; } }
+
+        public GiantPlanet(string name, double diameter, double mass, string type) : base(name, diameter, mass)
+        {
+            _type = type;
+        }
+    }
+}
